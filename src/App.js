df931c8a3203d0d8dec2ffcedc8ddad7086390ef
Kanha Dwivedi <madhurdwivedi123@gmail.com>
2024-01-13 07:12:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
-//import About from './Components/About';
+import About from './Components/About';
 import React, {useState} from 'react'; 
 import Alert from './Components/Alert';
-// import {
-//   BrowserRouter as Router,
-//   Switch,
-//   Route, 
-// }from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route, 
+}from "react-router-dom";
  
 function App() {
   const [mode, setMode] = useState('light'); //whether dark mode is enabled or not
@@ -22,7 +22,18 @@ function App() {
       setAlert(null);
     }, 1500);
   }
-  const toggleMode = ()=>{
+
+  // const removeBodyClasses = ()=>{
+  //   document.body.classList.remove('bg-light')
+  //   document.body.classList.remove('bg-dark')
+  //   document.body.classList.remove('bg-warning')
+  //   document.body.classList.remove('bg-danger')
+  //   document.body.classList.remove('bg-success')
+  // }
+  const toggleMode = (cls)=>{
+    // removeBodyClasses();
+    // console.log(cls)
+    // document.body.classList.add('bg-'+cls)
     if(mode === 'light'){
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
@@ -44,26 +55,26 @@ function App() {
   }
   return (
     <>
-    {/* <Router> */}
+    <Router>
     {/*<Navbar title= "Textutils" aboutText= "About textutils"/>*/}
     {/*<Navbar/>*/}
     <Navbar title= "Textutils" mode={mode} toggleMode={toggleMode} /> 
     <Alert alert={alert}/>
     <div className="container my-3">
-    {/* <Switch> */}
+    <Switch>
     {/*/users --> component 1
     /users/home --> component 2*/}
     {/*yahan per humne exact esliye use kiya hai because react partial matching karta hai and humein exact matching karane ke liye means ki home per jana hai ya users per ye dono
     alag alag hain then*/}
-          {/* <Route exact path="/about" element={<About/>}>
+          <Route exact path="/about" element={<About/>}>
             <About mode={mode} />
           </Route>
-          <Route exact path="/"> */}
+          <Route exact path="/">
             <TextForm showAlert = {showAlert} heading="Textutils - Word counter, character counter, Remove extra spaces" mode={mode}/>
-          {/* </Route>
-    </Switch> */}
+          </Route>
+    </Switch>
     </div>
-    {/* </Router> */}
+    </Router>
     </>
   ); 
 }
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -24,12 +24,12 @@ export default function Textform(props) {
       }
       const handleCopy = () => {
         //console.log("I am copy");
-        var text = document.getElementById("mybox");
-        text.select();
-        navigator.clipboard.writeText(text.value);
+        // var text = document.getElementById("mybox");
+        // text.select();
+        navigator.clipboard.writeText(text);
 
-        //eski help se text jo select hoga woh highlight nahi hoga bas copy ho jayega 
-        document.getSelection().removeAllRanges();
+        //eski help se text jo select hoga woh highlight nahi hoga bas copy ho jayega basically deselect ho jayega
+        //document.getSelection().removeAllRanges();
         
         props.showAlert("Copied to clipboard", "success")
       }
@@ -55,7 +55,7 @@ export default function Textform(props) {
               </div>
               <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
                 <h2>your text summary</h2>
-                <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+                <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
                 <p>{(0.008) * text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes read</p>
                 <h2>Preview</h2>
                 <p>{text.length>0?text:"Nothing to preview!"}</p>
